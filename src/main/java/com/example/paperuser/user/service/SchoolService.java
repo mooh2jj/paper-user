384c958b5c4d3109870f4d8614aaa434bf3e267b
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-08-01 23:16:59 +0900

    test2(학교를 수정한다.) ~ test4(지역으로 학교 목록을 가져온다.) 구현 완료

--- src/main/java/com/example/paperuser/user/service/SchoolService.java
+++ src/main/java/com/example/paperuser/user/service/SchoolService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -25,7 +26,19 @@ public class SchoolService {
         return schoolRepository.save(school);
     }
 
+    public Optional<School> updateName(Long schoolId, String name) {
+        return schoolRepository.findById(schoolId).map(school -> {
+            school.setName(name);
+            schoolRepository.save(school);
+            return school;
+        });
+    }
+
     public List<String> cities() {
         return schoolRepository.getCities();
+    }
+
+    public List<School> findAllByCity(String city) {
+        return schoolRepository.findByCity(city);
     }
 }
--- src/test/java/com/example/paperuser/user/service/SchoolServiceTest.java
+++ src/test/java/com/example/paperuser/user/service/SchoolServiceTest.java
@@ -21,31 +21,69 @@ class SchoolServiceTest {
 
     private SchoolService schoolService;
     private SchoolTestHelper schoolTestHelper;
+    School school;
 
     @BeforeEach
     void before() {
+        this.schoolRepository.deleteAll();  // 미리 깨끗이 지우고 시작하는 게 좋음!
         this.schoolService = new SchoolService(schoolRepository);
         this.schoolTestHelper = new SchoolTestHelper(this.schoolService);
+        school = this.schoolTestHelper.createSchool("test school", "seoul");
     }
 
     @DisplayName("1. 학교를 생성한다.")
     @Test
-    void save() {
+    void test_1() {
 //        School school = School.builder()
 //                .name("test school")
 //                .city("seoul")
 //                .build();
 //        schoolService.save(school);
-        School school = this.schoolTestHelper.createSchool("test school", "seoul");
+//        School school = this.schoolTestHelper.createSchool("test school", "seoul");
 
         var list = schoolRepository.findAll();
         assertEquals(1, list.size());
-        assertEquals("test school", list.get(0).getName());
-        assertEquals("seoul", list.get(0).getCity());
+//        assertEquals("test school", list.get(0).getName());
+//        assertEquals("seoul", list.get(0).getCity());
+        SchoolTestHelper.assertSchool(list.get(0), "test school", "seoul");
         System.out.println("save_sccuess!");
     }
 
+    @DisplayName("2. 학교를 수정한다.")
     @Test
-    void cities() {
+    void test_2() {
+        schoolService.updateName(school.getSchoolId(), "test2 school");
+        var list = schoolRepository.findAll();
+        assertEquals("test2 school", schoolRepository.findAll().get(0).getName());
+        System.out.println("test2_sccuess!");
+    }
+
+    @DisplayName("3. 학교를 목록을 가져온다.")
+    @Test
+    void test_3() {
+        var list = schoolService.cities();
+        assertEquals(1, list.size());
+        assertEquals("seoul", list.get(0));
+
+        schoolTestHelper.createSchool("busan school", "busan");
+        list = schoolService.cities();
+        assertEquals(2, list.size());   // busan 추가 -> size :2
+
+        System.out.println("test3_sccuess!");
+    }
+
+    @DisplayName("4. 지역으로 학교 목록을 가져온다.")
+    @Test
+    void test_4() {
+
+        var list = schoolService.findAllByCity("seoul");
+        assertEquals(1, list.size());
+
+        schoolTestHelper.createSchool("seoul2 school", "seoul");    // seoul지역에 학교 seoul2 school 추가 -> size:2
+        list = schoolService.findAllByCity("seoul");
+        assertEquals(2, list.size());
+
+
+        System.out.println("test4_sccuess!");
     }
 }
